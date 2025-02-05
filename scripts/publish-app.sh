9607fab4e3b4eb1904e9e80a9861b6dd87f6002f
siddhantCodes <siddhantk232@gmail.com>
2025-02-05 17:57:25 +0530

    Fix(CI): build js files before uploading to FT

--- scripts/publish-app.sh
+++ scripts/publish-app.sh
@@ -10,8 +10,16 @@ rm .gitignore
 
 echo .packages > .gitignore
 echo .fastn >> .gitignore
+echo node_modules >> .gitignore
 echo .is-local >> .gitignore
 
 sh -c "$(curl -fsSL https://fastn.com/install.sh)"
 
-cd lets-talk.fifthtry.site/ && fastn upload lets-talk
+cd lets-talk.fifthtry.site/
+
+cd js
+npm install
+npm run build
+cd ..
+
+fastn upload lets-talk
