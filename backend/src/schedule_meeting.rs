2f50949dac7abaab746d11675188cb0c7f1a91ae
Ayush Deolasee <pypdeveloper@deolasee.org>
2025-05-03 12:26:02 +0530

    working on schedule_meeting.rs file

--- backend/src/schedule_meeting.rs
+++ backend/src/schedule_meeting.rs
@@ -1,100 +1,5 @@
-// // #[ft_sdk::form]
-
-// // fn schedule_meeting(
-// //     title: ft_sdk::Required<"title">,
-// //     attendies: ft_sdk::RequiredUsers,
-// //     user: crate::auth::RequiredUser,
-// //     host: ft_sdk::Host,
-// //     config: crate::Config,
-// // ) -> ft_sdk::form::Result {
-// //     if !user.is_special(&config) {
-// //         return Err(title
-// //             .error("You are not authorized to create a meeting")
-// //             .into());
-// //     }
-
-// //     let meeting = crate::dyte::create_meeting(&title)?;
-// //     let name = if user.name.is_empty() {
-// //         None
-// //     } else {
-// //         Some(user.name.as_str())
-// //     }
-
-// //     let preset = config.preset_host;
-
-// //     let username = crate::dyte::Username::new(user.username, &host);
-// //     let participant = crate::dyte::add_participant(&meeting.data.id, %preset, name, username);
-
-// //     let session_cookie = crate::create_session_cookie(
-// //         &participant.data.token,
-// //         &meeting.data.id,
-// //         &host,
-// //         config.secure_sessions
-// //     )
-// // }
-
-// #[ft_sdk::form]
-// /// Create a meeting and add the user to it as a participant
-// fn schedule_meeting(
-//     title: ft_sdk::Required<"title">,
-//     date: ft_sdk::Required<"date">,
-//     // attendies: ft_sdk::Required<"attendies">,
-//     user: crate::auth::RequiredUser,
-//     host: ft_sdk::Host,
-//     config: crate::Config,
-//     app_url: ft_sdk::AppUrl,
-//     scheme: crate::HTTPSScheme,
-// ) -> ft_sdk::form::Result {
-//     println!("Scheduling meeting rust file ran!");
-//     // if !user.is_special(&config) {
-//     //     return Err(title
-//     //         .error("You are not authorized to create a meeting")
-//     //         .into());
-//     // }
-
-//     // let meeting = crate::dyte::create_meeting(&title)?;
-//     // let name = if user.name.is_empty() {
-//     //     None
-//     // } else {
-//     //     Some(user.name.as_str())
-//     // };
-
-//     // let date_parts: Vec<&str> = date.split('|').collect();
-//     // let start_date = date_parts[0];
-//     // let end_date = date_parts[1];
-
-
-
-//     // let preset = config.preset_host;
-
-//     // ft_sdk::println!("Using preset: {preset} to schedule a meeting");
-//     // ft_sdk::println!("Scheduled meeting for {date}");
-//     // // ft_sdk::println!("Attendies for scheduled meeting for {attendies}"); 
-//     // let username = crate::dyte::Username::new(user.username, &host);
-//     // let participant = crate::dyte::add_participant(&meeting.data.id, &preset, name, username)?;
-
-//     // let session_cookie = crate::create_session_cookie(
-//     //     &participant.data.token,
-//     //     &meeting.data.id,
-//     //     &host,
-//     //     config.secure_sessions,
-//     // )?;
-
-//     // // lets-talk.fifthtry.site/meeting.ftd
-//     // let app_url = crate::temp_fix_app_url(app_url);
-//     // let meeting_page_url = app_url.join(&scheme, &host, "meeting")?;
-
-//     // Ok(
-//     //     ft_sdk::form::redirect(format!("{meeting_page_url}{}", meeting.data.id))?
-//     //         .with_cookie(session_cookie),
-//     // )
-// }
-
-// // fn create_ics_file()
-
-
 #[ft_sdk::form]
-/// Create a meeting and add the user to it as a participant
+
 fn schedule_meeting(
     title: ft_sdk::Required<"title">,
     start_date: ft_sdk::Required<"start_datetime">,
@@ -106,18 +11,22 @@ fn schedule_meeting(
     app_url: ft_sdk::AppUrl,
     scheme: crate::HTTPSScheme,
 ) -> ft_sdk::form::Result {
-    ft_sdk::println!("Start Date:");
-    ft_sdk::println!("{}", start_date);    
+    // Uncommeneted for debugging reasons
+    // if !user.is_special(&config) {
+    //     return Err(title
+    //         .error("You are not authorized to create a meeting")
+    //         .into());
+    // }
+   
+    let output = return_ics_file("ayush@deolasee.org, somethingelse@gmail.com");    
     
-    ft_sdk::println!("End Date:");
-    ft_sdk::println!("{}", end_date);    
-        
-    if !user.is_special(&config) {
-        return Err(title
-            .error("You are not authorized to create a meeting")
-            .into());
+    if output.is_err() {
+        // Logic to handle the error
+        ft_sdk::println!("Some Error was encountered");
     }
-    // ft_sdk::println!("{}", start_date);
+    
+    ft_sdk::println!("Output: {:?}", output);
+
     let meeting = crate::dyte::create_meeting(&title)?;
 
     let name = if user.name.is_empty() {
@@ -144,7 +53,34 @@ fn schedule_meeting(
     let app_url = crate::temp_fix_app_url(app_url);
     let meeting_page_url = app_url.join(&scheme, &host, "meeting")?;
     Ok(
-        ft_sdk::form::redirect(format!("{meeting_page_url}{}", meeting.data.id))?
-            .with_cookie(session_cookie),
+        ft_sdk::form::redirect("")?
+        // Removed for debugging reasons 
+        // ft_sdk::form::redirect(format!("{meeting_page_url}{}", meeting.data.id))?
+        //     .with_cookie(session_cookie),
     )
+}
+
+// meeting_name &str, start_date: &str, end_date: &str
+fn return_ics_file(meeting_name: &str, meeting_url: &str, attendees: &str) -> std::io::Result<()> {
+    let mut ics_string = String::new(); 
+    let mut attendees_list = Vec::<String>::new();
+    for attendee in attendees.split(',') {
+        let attendee = attendee.trim();
+        if !attendee.is_empty() {
+            if !attendee.contains('@') {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::InvalidInput,
+                    format!("Invalid Email address: {}", attendee),
+                ));
+            } else {
+                attendees_list.push(attendee.to_string());
+            }
+        }
+    }
+
+    for attendee in attendees_list {
+        
+        ft_sdk::println!("Attendee: {}", attendee);
+    }
+    Ok(())
 }
