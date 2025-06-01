f8555e5ca6c812d424594a46e1f6bcf13dd7718a
Ayush Deolasee <pypdeveloper@deolasee.org>
2025-06-01 20:35:44 +0530

    updating schedule_meeting.rs file to properly handle input data

--- backend/src/schedule_meeting.rs
+++ backend/src/schedule_meeting.rs
@@ -19,8 +19,6 @@ fn schedule_meeting(
     //         .into());
     // }
 
-    ft_sdk::println!("start_datetime: {} | end_datetime: {}", start_datetime, end_datetime);
-
     let meeting = crate::dyte::create_meeting(&title)?;
 
     let name = if user.name.is_empty() {
@@ -34,7 +32,7 @@ fn schedule_meeting(
     ft_sdk::println!("Using preset: {preset} to create meeting");
 
     let username = crate::dyte::Username::new(user.username, &host);
-    let participant = crate::dyte::add_participant(&meeting.data.id, &preset, name, username)?;
+    let participant: crate::dyte::DyteResponse<crate::dyte::DyteAddParticipant> = crate::dyte::add_participant(&meeting.data.id, &preset, name, username)?;
     ft_sdk::println!("Meeting.data.id: {:?}", &meeting.data.id);
     let session_cookie = crate::create_session_cookie(
         &participant.data.token,
@@ -42,26 +40,42 @@ fn schedule_meeting(
         &host,
         config.secure_sessions,
     )?;
-
     // lets-talk.fifthtry.site/meeting.ftd
     let app_url = crate::temp_fix_app_url(app_url);
     let meeting_page_url = app_url.join(&scheme, &host, "meeting")?;
-    let start_date_debug = "20250625T100000Z";
-    let end_date_debug = "20250625T100000Z";
-    let organizer_email = "something@gmail.com";
-    let uuid = crate::uuid::gen_uuid_with_xorshift(0.1);
-     
-    let output = return_ics_file(
+    let uuid: String = crate::uuid::gen_uuid_with_xorshift(0.1);
+
+    let mut attendees_list = Vec::<String>::new();
+    let organizer_email = user.email.to_string(); 
+
+    for attendee in attendees.split(',') {
+        let attendee = attendee.trim();
+        if !attendee.is_empty() {
+            if !attendee.contains('@') {
+                // Return an error here
+                ft_sdk::println!("Invalid Email Address")
+            } else {
+                attendees_list.push(attendee.to_string());
+            }
+        }
+    }
+
+    if attendees_list.is_empty() && !attendees.trim().is_empty() {
+        // Return an error here
+        ft_sdk::println!("Now Email Address input")
+   }
+
+   let ics_file = return_ics_file(
         &title, 
         &meeting_page_url, 
-        "ayush@deolasee.org, somethingelse@gmail.com, nothing@nothing", 
+        attendees_list.clone(),
         uuid,
-        start_date_debug.to_string(), 
-        end_date_debug.to_string(), 
-        organizer_email.to_string() 
+        start_datetime.to_string(), 
+        end_datetime.to_string(), 
+        organizer_email
     );
     
-    
+
     Ok(
         ft_sdk::form::redirect("")?
         // Removed for debugging reasons 
@@ -70,30 +84,9 @@ fn schedule_meeting(
     )
 }
 
-fn return_ics_file(meeting_title: &str, meeting_url: &str, attendees: &str, uid: String, start_datetime: String, end_datetime: String, organizer_email: String) -> std::io::Result<String> {
+fn return_ics_file(meeting_title: &str, meeting_url: &str, attendees_list: Vec<String>, uid: String, start_datetime: String, end_datetime: String, organizer_email: String) -> String {
     let mut ics_string = String::new(); 
-    let mut attendees_list = Vec::<String>::new();
     
-    for attendee in attendees.split(',') {
-        let attendee = attendee.trim();
-        if !attendee.is_empty() {
-            if !attendee.contains('@') {
-                return Err(std::io::Error::new(
-                    std::io::ErrorKind::InvalidInput,
-                    format!("Invalid Email address: {}", attendee),
-                ));
-            } else {
-                attendees_list.push(attendee.to_string());
-            }
-        }
-    }
-
-    if attendees_list.is_empty() && !attendees.trim().is_empty() {
-        return Err(std::io::Error::new(
-           std::io::ErrorKind::InvalidInput,
-           "No valid attendee email addresses provided.",
-       ));
-   }
 
     ics_string.push_str(format!(
         "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:fifthtry/lets-talk\nCALSCALE:GREGORIAN\nMETHOD:REQUEST\nBEGIN:VEVENT\nUID:{uid}\nDISTAMP:{start_datetime}\nSUMMARY:{meeting_title}\nLOCATION:{meeting_url}\nDTSTART:{start_datetime}\nDTEND:{end_datetime}\nORGANIZER:mailto:{organizer_email}\n"
@@ -108,7 +101,7 @@ fn return_ics_file(meeting_title: &str, meeting_url: &str, attendees: &str, uid:
         "SEQUENCE:0\nTRANSP:OPAQUE\nEND:VEVENT\nEND:VCALENDAR"
     ).as_str());
 
-    Ok(ics_string)
+    return ics_string;
 }
 
 // async fn send_calendar_invite(ics_content: String, organizer_email: &str, attendee_email: &str, meeting_title: &str) -> std::io::Result<()> {
